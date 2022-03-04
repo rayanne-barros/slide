6029e83ed2865ef82e2ca901718261fe9cb92758
Rayanne Barros <rayanneb126@gmail.com>
2022-03-04 01:03:51 -0300

    Navegação prev e next finalizada

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-import Slide from './slide.js';
+import { SlideNav } from './slide.js';
 
-const slide = new Slide('.slide', '.slide-wrapper');
+const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 
-console.log(slide)
+slide.addArrow('.prev', '.next')
 
-slide.changeSlide(1);
-slide.activePrevSlide();
+console.log(slide)
